50e150cb9946b587855c43317b0fc890723d07ad
C22454222 <124152728+C22454222@users.noreply.github.com>
2024-03-14 11:56:50 +0000

    First test commit

---
+++ java/src/C22454222/ChrisVisual.java
@@ -0,0 +1,10 @@
+package C22454222; // Assuming your package name is C22454222
+
+// Import the Visual class from ie.tudublin package
+import ie.tudublin.*;
+import processing.core.PApplet;
+
+// Create a subclass named MyVisual that extends Visual
+public class ChrisVisual extends Visual {
+    // Your subclass code here
+}
