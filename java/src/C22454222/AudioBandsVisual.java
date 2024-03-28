29d2808a7dadba1c41d6bc0a8894d0170480c3c6
C22454222 <124152728+C22454222@users.noreply.github.com>
2024-03-28 13:03:15 +0000

    Made changes to MainVisual and AudioBandsVisual .java was made

---
+++ java/src/C22454222/AudioBandsVisual.java
@@ -0,0 +1,18 @@
+package C22454222;
+
+import ie.tudublin.*;
+
+public class AudioBandsVisual extends Visual{
+
+    MainVisual mv;
+
+    public AudioBandsVisual(MainVisual mv){
+        this.mv = mv;
+    }
+
+    public void render()
+    {
+        
+    }
+
+}
--- java/src/C22454222/MainVisual.java
+++ java/src/C22454222/MainVisual.java
@@ -1,5 +1,24 @@
 package C22454222;
 
-public class MainVisual {
+import ie.tudublin.*;
+
+public class MainVisual extends Visual{
+    int visualSwap = 0;
+    int playTune = 0;
+    int stopPlay = 0;
+    public float eRadius;
+
+    // Initialize each visual object
+
+    /* 
+
+    AimeeVisual.java aimee;
+    NeilVisual.java neil;
+    JadeVisual.java jade;
+    JasonVisual.java jason;
+    ChrisVisual.java chris;
+    AudioBandsVisual.java audioBandsVisual;
+
+    */
     
 }
