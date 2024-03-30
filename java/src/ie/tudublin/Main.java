3fb6de2bfffa2cca61114ce1c81ae1fccb411938
C22454222 <124152728+C22454222@users.noreply.github.com>
2024-03-30 00:43:17 +0000

    a

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,8 +9,6 @@ public class Main {
         MainVisual visualizer = new MainVisual();
         String[] a = { "MAIN" };
         processing.core.PApplet.runSketch(a, visualizer);
-        String[] b = {"Second"};
-        processing.core.PApplet.runSketch(b, new MainVisualMenu(visualizer));
     }
 
     public static void main(String[] args) {
