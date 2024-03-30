020286651b96b35be7ee700b4b86deb206f8ae47
C22454222 <C22454222@mytudublin.ie>
2024-03-30 14:01:34 +0000

    Creating visual menu

--- java/src/C22454222/MainVisualMenu.java
+++ java/src/C22454222/MainVisualMenu.java
@@ -1,5 +1,42 @@
 package C22454222;
 
-public class MainVisualMenu {
+import ie.tudublin.*;
+
+public class MainVisualMenu extends Visual{
+
+    MainVisual visualMode;
+
+public MainVisualMenu(MainVisual visualMode){
+    this.visualMode = visualMode;
+}
+
+public int options = 5;
+int boxHeight = 100;
+int boxWidth = 200;
+
+public void settings()
+{
+    size(400,450);
+}
+
+public void setup()
+{
+    colorMode(HSB);
+}
+
+public void keyPressed()
+{
+    visualMode.visualSwap = (visualMode.visualSwap + 1) % options;
+}
+
+
+
+
+
+
+
+
+
+
 
 }
