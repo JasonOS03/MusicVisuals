eb6071bdc139a1a0125b54009fba3375e3e97804
C22454222 <C22454222@mytudublin.ie>
2024-03-31 16:06:22 +0100

    Setting up my own visual methods

--- java/src/C22454222/ChrisVisual.java
+++ java/src/C22454222/ChrisVisual.java
@@ -1,22 +1,44 @@
 package C22454222;
 
 import ie.tudublin.*;
-import processing.core.PApplet;
+// import processing.core.PApplet;
 
 public class ChrisVisual extends Visual 
 {
-    MainVisual mainVisual;
+    MainVisual mv;
 
     public ChrisVisual(MainVisual mv) 
     {
-        this.mainVisual = mv;
+        this.mv = mv;
     }
 
     public void render()
     {
+        mv.beat.detect(mv.as.mix);
+        mv.beat.detectMode(0);
+        mv.background(0);
+        mv.textSize(25);
+        mv.fill(255);
+        mv.textAlign(CENTER,CENTER);
+
+        mv.fCounter++;
+
+        mv.translate(mv.width/2, mv.height/2);
+
+        if (mv.chrisOption == 1)
+        {
+
+        }
+
+        if (mv.chrisOption == 2)
+        {
+            
+        }
+        
+        if (mv.chrisOption == 3)
+        {
+            
+        }
 
     }
-
-  
-
 }
