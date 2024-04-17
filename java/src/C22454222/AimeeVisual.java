189e8e4ca8322627f7870777ff2fa759231cc2b8
aimee1111111 <124159644+aimee1111111@users.noreply.github.com>
2024-04-17 16:13:48 +0100

    setting up my visual

--- java/src/C22454222/AimeeVisual.java
+++ java/src/C22454222/AimeeVisual.java
@@ -1,3 +1,17 @@
-public class AimeeVisual {
-    
+package C22454222;
+import ie.tudublin.*;
+
+
+public class AimeeVisual extends Visual{
+    MainVisual mainVisual;
+
+    public AimeeVisual(MainVisual mainVisual){
+        this.mainVisual=mainVisual;
+    }
+
+
+
+    public void render(){
+
+    }
 }
