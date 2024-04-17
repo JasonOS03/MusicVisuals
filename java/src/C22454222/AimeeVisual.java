03fbad8b85c2361780a3602b878862f9b786d9af
aimee1111111 <124159644+aimee1111111@users.noreply.github.com>
2024-04-17 22:58:58 +0100

    setting up branch

--- java/src/C22454222/AimeeVisual.java
+++ java/src/C22454222/AimeeVisual.java
@@ -1,6 +1,26 @@
 package C22454222;
 import ie.tudublin.*;
 
+class Branch{
+    MainVisual mainVisual;
+    float start;
+    float amplitude =0;
+    float angle=0;
+
+
+    Branch[] branches;//branch array 
+
+    Branch (MainVisual mainVisual,float start, float amplitude,float angle ,int branches  ){
+        this.mainVisual=mainVisual;
+        this.start = start;
+        this.amplitude=amplitude;
+        this.angle=angle;
+
+        branch(branches);
+    }
+
+}
+
 
 public class AimeeVisual extends Visual{
     MainVisual mainVisual;
