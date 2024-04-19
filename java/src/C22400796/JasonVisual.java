0314e79013124e9d903a6600f8623441d6f40711
JasonOS03 <C22400796@mytudublin.ie>
2024-04-19 12:41:27 +0100

    adding objects in the center

--- java/src/C22400796/JasonVisual.java
+++ java/src/C22400796/JasonVisual.java
@@ -70,13 +70,13 @@ public class JasonVisual extends PApplet
     }
     float average = sum / (float) ab.size();
     float amplitude = average * 800;    
-    float smoothrotation = lerp(0,radians(amplitude),0.2f);
+    float smoothrotation = lerp(0,radians(amplitude),0.6f);
     float CenterY = height/2;
     float CenterX = width/2;
 
     translate(width/2,height/2);
     
-    int num_diagonals = (int) map(amplitude, 0, 1, 1, 3);
+    int num_diagonals = (int) map(amplitude, 0, 1, 1, 8);
     
 
     
@@ -95,8 +95,9 @@ public class JasonVisual extends PApplet
     }
     stroke(122,100,100);
     noFill();
+    rotate(smoothrotation);
     strokeWeight(4);
-    ellipse(0,0,CenterX/2,CenterY/2);
+    ellipse(0,0,CenterX/2,amplitude);
     
     
     
