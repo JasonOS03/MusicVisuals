2655ad15645cd16beb11e91be6f617bc35923dd8
c22394466 <124158789+c22394466@users.noreply.github.com>
2024-04-23 14:47:16 +0100

    Merge successful

--- java/src/C22454222/MainVisual.java
+++ java/src/C22454222/MainVisual.java
@@ -9,6 +9,7 @@ public class MainVisual extends Visual
     int visualSwap = 0; // Variable to control visual swapping
     int playTune = 0; // Variable to control playing tune
     int stopPlay = 0; // Variable to control stopping playback
+    public float eRadius = 20;
 
     // AimeeVisual aimee;
     // NeilVisual neil;
@@ -16,14 +17,15 @@ public class MainVisual extends Visual
     // JasonVisual jason;
     // ChrisVisual chris;
     AudioBandsVisual audioBandsVisual; // Instance of AudioBandsVisual class
+
     public float fCounter = 0; // Variable to store a floating-point counter value
     public int chrisOption = 1; // Variable to store an option for Chris's visual
 
     // Method to set initial settings
     public void settings() 
     {
-        size(1024, 500); // Setting window size
-        // fullScreen();
+        // Setting window size
+        fullScreen();
         // fullScreen(P3D,SPAN);
     }
 
@@ -34,10 +36,13 @@ public class MainVisual extends Visual
         loadAudio("Project.wav"); // Loading audio file
         BeatDetect(); // Initializing beat detection
         colorMode(HSB); // Setting color mode
+
         // aimee = new AimeeVisual(this);
         // neil = new NeilVisual(this);
         jade = new JadeVisual(this);
         // jason = new JasonVisual(this);
+
+        // chris = new ChrisVisual(this); // Initializing ChrisVisual instance with appropriate constructor
         audioBandsVisual = new AudioBandsVisual(this); // Initializing AudioBandsVisual instance
     }
 
