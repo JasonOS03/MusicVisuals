a008bbe98f48afd2d4251d8d1fea8cd99a82cba0
JasonOS03 <C22400796@mytudublin.ie>
2024-04-23 16:31:34 +0100

    Merged Jason-dev

--- java/src/C22454222/MainVisual.java
+++ java/src/C22454222/MainVisual.java
@@ -10,15 +10,14 @@ public class MainVisual extends Visual
     int visualSwap = 0; // Variable to control visual swapping
     int playTune = 0; // Variable to control playing tune
     int stopPlay = 0; // Variable to control stopping playback
-    /*
-     * AimeeVisual aimee;
-     * NeilVisual neil;
-     * JadeVisual jade;
-     * JasonVisual jason;
-     * ChrisVisual chris;
-     */
-    AudioBandsVisual audioBandsVisual; // Instance of AudioBandsVisual class
+    
+    AimeeVisual aimee;
     NeilVisual neil;
+    JadeVisual jade;
+    JasonVisual jason;
+    ChrisVisual chris;
+     
+    AudioBandsVisual audioBandsVisual; // Instance of AudioBandsVisual class
 
     public float fCounter = 0; // Variable to store a floating-point counter value
     public int branchCounter=0;
@@ -39,12 +38,12 @@ public class MainVisual extends Visual
         loadAudio("Project.wav"); // Loading audio file
         BeatDetect(); // Initializing beat detection
         colorMode(HSB); // Setting color mode
-        /*
-         * aimee = new AimeeVisual(this);
-         * neil = new NeilVisual(this);
-         * jade = new JadeVisual(this);
-         * jason = new JasonVisual(this);
-         */
+        
+        aimee = new AimeeVisual(this);
+        neil = new NeilVisual(this);
+        jade = new JadeVisual(this);
+        jason = new JasonVisual(this);
+        
         audioBandsVisual = new AudioBandsVisual(this); // Initializing AudioBandsVisual instance
         neil = new NeilVisual(this);
         
